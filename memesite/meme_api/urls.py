dbf947f8f1d9ca46e3e5e368a7b73b92163dd925
j096 <better_daum@naver.com>
2022-11-24 19:36:39 +0900

    검색 기능 추가

--- memesite/meme_api/urls.py
+++ memesite/meme_api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'memes'
 urlpatterns=[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('search/',views.search,name='search')
 ]
--- memesite/meme_api/views.py
+++ memesite/meme_api/views.py
@@ -10,6 +10,29 @@ def index(request):
     response = requests.get(url)
     #Convers json to dict
     result = response.json()
-    print(result['data'])
     template = loader.get_template('meme_api/index.html')
     return HttpResponse(template.render(result['data'],request))
+
+
+
+def search(request):
+    url = 'https://api.imgflip.com/get_memes'
+    response = requests.get(url)
+    
+    search = request.POST['searchVal']
+    res = json.loads(response.text)
+    arr1 = res.get('data').get('memes')
+    meme = []
+    result={}
+    result['searchVal']=search
+    for lists in arr1:
+        if search in lists['name']:
+                meme.append(lists)
+                
+    if(meme):
+        result['memes']=meme
+    
+        
+    print(result)
+    template = loader.get_template('meme_api/index.html')
+    return HttpResponse(template.render(result,request))
